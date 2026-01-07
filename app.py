b9663aa53067bb1441c4ccc3dbac77a881d4c318
Byte-Craftsman-Alpha <aditya463615@gmail.com>
2026-01-07 21:07:56 +0530

    UI update

--- app.py
+++ app.py
@@ -1441,4 +1441,4 @@ if __name__ == '__main__':
             db.session.add(admin_user)
             db.session.commit()
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.31.138', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
